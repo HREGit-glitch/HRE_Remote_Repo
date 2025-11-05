d783f8c0185fa3560279f3f64bc69c989786dc76
hosny <hosnyramdan1997@gmail.com>
2025-11-05 20:23:18 +0200

    Stage and Commit and push it to remote

--- HelloGitPythonMasterLocalRepo.py
+++ HelloGitPythonMasterLocalRepo.py
@@ -1,4 +1,8 @@
 print("Hello python world master branch ")
 print("Hello python world test branch ")
 print("Hello python  Ok")
-print("I edited from Remote Repo  Ok")
+print("I edited from Remote Repo  Ok") #git pull HRE_Remote_Repo_Origin main
+#git add .
+#git commit -m "Stage and Commit and push it to remote "
+#git push
+print(" Stage and Commit and push it to remote  Ok")
