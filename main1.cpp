5c78a7a019380fa5c595d27750c0e64fee34d381
hosny <hosnyramdan1997@gmail.com>
2025-11-05 20:59:08 +0200

    Add C++ File

---
+++ main1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+cout<<"Hello C++" ;
+
+    return 0 ; 
+}
+
+/*
+1- compile  main1.cpp  file and   compile  sourcefile.cpp  file 
+g++ main1.cpp  Employee.cpp -o main1
+2- run program 
+./main1
+
+*/
